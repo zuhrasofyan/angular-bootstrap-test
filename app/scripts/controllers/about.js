699e10b8af5b9766330bc8d2fab4a8497e6d5abe
Zuhra Sofyan <zuhra.sofyan@gmail.com>
2015-08-26 13:59:32 +0200

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBootstrapTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularBootstrapTestApp
+ */
+angular.module('angularBootstrapTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
